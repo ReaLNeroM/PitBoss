806557456e24aef92dfb8bd242332e277f2e0524
Vladimir Maksimovski <vlade.maksimovski@gmail.com>
2020-08-09 03:51:11 -0400

    Add build

---
+++ pitboss-client/build/precache-manifest.75934f3fd201cdb534034a8f3d2a9aa2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "165322aeb6fb803d3c76959d2d59ad5f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bb73daf0232ee29b6345",
+    "url": "/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "b1e6be35a6403c07178c",
+    "url": "/static/css/main.70cc8e9b.chunk.css"
+  },
+  {
+    "revision": "bb73daf0232ee29b6345",
+    "url": "/static/js/2.9182ac74.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.9182ac74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1e6be35a6403c07178c",
+    "url": "/static/js/main.4f5e81e9.chunk.js"
+  },
+  {
+    "revision": "bc91cd53ad5408702ee6",
+    "url": "/static/js/runtime-main.2a68516a.js"
+  }
+]);
