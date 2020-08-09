d6ff74057777ba685fac84c00490758cea742673
Vladimir Maksimovski <vlade.maksimovski@gmail.com>
2020-08-09 04:13:24 -0400

    Updates

---
+++ precache-manifest.34e9d444c95972ffb697f093766cf9ff.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1b90c91d647291ac0e14f780084427f6",
+    "url": "/PitBoss/pitboss-client/build/index.html"
+  },
+  {
+    "revision": "bb73daf0232ee29b6345",
+    "url": "/PitBoss/pitboss-client/build/static/css/2.11829350.chunk.css"
+  },
+  {
+    "revision": "033aa08ff8b62e7131a9",
+    "url": "/PitBoss/pitboss-client/build/static/css/main.70cc8e9b.chunk.css"
+  },
+  {
+    "revision": "bb73daf0232ee29b6345",
+    "url": "/PitBoss/pitboss-client/build/static/js/2.9182ac74.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/PitBoss/pitboss-client/build/static/js/2.9182ac74.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "033aa08ff8b62e7131a9",
+    "url": "/PitBoss/pitboss-client/build/static/js/main.cd0ac95b.chunk.js"
+  },
+  {
+    "revision": "6a10b2f34553245fca1f",
+    "url": "/PitBoss/pitboss-client/build/static/js/runtime-main.c0f12c31.js"
+  }
+]);
